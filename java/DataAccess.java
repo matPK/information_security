8bec1b64d2deae445bd731322f0854f1d2d118a9
Matheus Adorni Dardenne <mat.dardenne@gmail.com>
2016-03-06 14:45:44 -0300

    Update

--- java/DataAccess.java
+++ java/DataAccess.java
@@ -18,18 +18,18 @@ public class DataAccess {
 	private Connection connect(){
 		try{
 			return DriverManager.getConnection("jdbc:mysql://localhost:3306/DB_NAME_HERE?autoReconnect=true&useSSL=false", "LOGIN","PASSWORD");
-			
 		}catch(Exception e){
 			e.printStackTrace();
 			return null;
 		}
 	}
+	
+	//query
 	public static ResultSet dataAccessQuery(String query){
 		try{
 			Connection c = getInstance().connect();
 			Statement myStat = c.createStatement();
-			ResultSet myRs = myStat.executeQuery(query);
-			return myRs;
+			return myStat.executeQuery(query);
 		}catch(Exception e){
 			e.printStackTrace();
 			return null;
