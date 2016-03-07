b47b3f47874bb957b2e33adaa02ec76fe1949040
Matheus Adorni Dardenne <mat.dardenne@gmail.com>
2016-03-07 00:24:24 -0300

    Improved
    Added getStatement and other minor chances.

--- java/DataAccess.java
+++ java/DataAccess.java
@@ -18,31 +18,45 @@ public class DataAccess {
 	//db connection method
 	private Connection connect(){
 		try{
-			return DriverManager.getConnection("jdbc:mysql://localhost:3306/DB_NAME_HERE?autoReconnect=true&useSSL=false", "LOGIN","PASSWORD");
+			return DriverManager.getConnection(
+				"jdbc:mysql://localhost:3306"+//db address
+				"/DB_NAME_HERE"+//name of the db
+				"?autoReconnect=true"+//autoReconnect?
+				"&useSSL=false",//use SSL?
+				"LOGIN",//insert your db login here
+				"PASSWORD"//insert your password here
+			);
 		}catch(Exception e){
 			e.printStackTrace();
 			return null;
 		}
 	}
 	
-	//query executer, returns result set. Use for SELECT.
+	//returns a statement created within the connection variable
+	private static Statement getStatement(){
+		try{
+			Connection c = getInstance().connect();
+			return c.createStatement();
+		}catch(Exception e){
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	//query executer, returns result set. Use for SELECT. Pass the String of the query as the parameter.
 	public static ResultSet dataAccessQuery(String query){
 		try{
-			Connection c = getInstance().connect();
-			Statement myStat = c.createStatement();
-			return myStat.executeQuery(query);
+			return getStatement().executeQuery(query);
 		}catch(Exception e){
 			e.printStackTrace();
 			return null;
 		}
 	}
 	
-	//update executer, returns nothing. Use for DELETE, UPDATE, etc.
+	//update executer, returns nothing. Use for DELETE, UPDATE, etc. Pass the String of the query as the parameter.
 	public static void nullDataAccessQuery(String query){
 		try{
-			Connection c = getInstance().connect();
-			Statement myStat = c.createStatement();
-			myStat.executeUpdate(query);
+			getStatement().executeUpdate(query);
 		}catch(Exception e){
 			e.printStackTrace();
 		}
